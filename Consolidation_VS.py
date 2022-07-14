b87dfaf6752cafb6a72fe6e2186eccb307571476
yongpengfu <yongpengfu0011@gmail.com>
2022-07-14 13:31:35 -0600

    multiple store consolidation

--- Consolidation_VS.py
+++ Consolidation_VS.py
@@ -7,20 +7,6 @@ from collections import defaultdict
 import os
 from datetime import date
 
-#read in the excel sheet
-#NOTE: when you try to read excel in python, you should close any excel file you want to load first
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
-Sending_Info = pd.read_excel(file_path, sheet_name = "Sending Info")
-PA_Info = pd.read_excel(file_path, sheet_name = "PA Info")
-#group same Sku together for both info sheet
-Sending_Info_group = Sending_Info.groupby(['Sending_Store', 'Comm', 'Style','Sku','Region'],as_index = False)['OH'].sum()
-#remove the sending store id in the PA_Info sheet
-PA_Info = PA_Info.loc[~PA_Info.PA_Store.isin(set(Sending_Info['Sending_Store']))]
-PA_Info_group = PA_Info.groupby(['PA_Store', 'Comm', 'Style','Sku','Region'],as_index = False)['PA for the year'].sum()
-
-#create a store class so that once one store get transferred, the master sheet is modified at the same time
 #we need to remove all the sending store id in the receiving store B list
 
 class Store:
@@ -62,8 +48,10 @@ class Store:
         #the rank of each store based on the total capacity of a store and weighted with the percentage of matching number
         #this variable has excluded the sending store ID (we dont need the rank of itself)
         #NOTE I only calculate the stores that have the same Region as sending store
-        self.store_rank = (((Store.store_capacity[list(set(Store.all_store.loc[Store.all_store.Region == self.one_store.loc[0, 'Region']].PA_Store))]) * \
-                            (self.matching_series/self.matching_series.sum())).sort_values(ascending=False))
+        #what if there is no match at all, and what if some of them are matching 0, some of them have partial match
+        self.store_rank = (((Store.store_capacity[list(set(self.all_store_same_region.PA_Store))]) * \
+                            ((self.matching_series/self.matching_series.sum()).where(~(self.matching_series/self.matching_series.sum()).isin([0,np.nan,-np.inf, np.inf]),0.0000001))).sort_values(ascending=False))
+
     #when a list of skus from store (A) are transferred to another store (B):
     #(1) update the SKU (PA for the year in store B) with the corresponding OH from A store
     #(2) change the OH for that SKU in this store (A) to 0
@@ -79,7 +67,7 @@ class Store:
         #then update the all_store_same_region as well based on the new all_store
         self.all_store_same_region = Store.all_store.loc[Store.all_store.Region == self.one_store.loc[0, 'Region']]
         
-        #update the total capacity each receiving store can hold once it receives some items
+        #update the total capacity each receiving store can hold once it receives some items ??
         Store.store_capacity = Store.all_store.groupby('PA_Store')['PA for the year'].sum()
         #one particular store can only accpet 250 total transferred units
         Store.store_total_addition[store_id] += self.one_store[self.one_store.Sku.isin(sku_list)].set_index('Sku')['OH'].sum()
@@ -99,9 +87,12 @@ class Store:
         
         #this variable excludes the sending store ID because Store.all_store has dropped the senidng store ID (we dont need the rank of itself)
         #NOTE I only calculate the stores that have the same Region as sending store
-        self.store_rank = (((Store.store_capacity[list(set(Store.all_store.loc[Store.all_store.Region == self.one_store.loc[0, 'Region']].PA_Store))]) * \
-                            (self.matching_series/self.matching_series.sum())).sort_values(ascending=False))
-                            
+        #what if there is no match at all, and what if some of them are matching 0, some of them have partial match
+        #if there is no match (0) for individual store, or there is no match at all between sending stores and receiving stores (NaN)
+        #I will give them a really small weight, 0.0000001
+        self.store_rank = (((Store.store_capacity[list(set(self.all_store_same_region.PA_Store))]) * \
+                            ((self.matching_series/self.matching_series.sum()).where(~(self.matching_series/self.matching_series.sum()).isin([0,np.nan,-np.inf, np.inf]),0.0000001))).sort_values(ascending=False))
+
 #Transfer one sending store A to a list of receiving stores B
 def consolidation(sending_store, all_store):
     #(1): initiate the store object
@@ -127,30 +118,33 @@ def consolidation(sending_store, all_store):
             #and all the remaining MUST be transferred completely to next available highest receiving store B,
             #in other words, one sending store can only be split into 2 stores maximum
             else:
-                sending_sku_set = set(sending_store.one_store.loc[sending_store.one_store.OH > 0, 'Sku']) \
-                                        & set(sending_store.all_store_same_region.loc[(sending_store.all_store_same_region.PA_Store == sending_store.one_store.loc[0, 'Sending_Store']) & \
+                sending_sku_set = set(sending_store.one_store.loc[:, 'Sku']) \
+                                        & set(sending_store.all_store_same_region.loc[(sending_store.all_store_same_region.PA_Store == index) & \
                                              (sending_store.all_store_same_region['PA for the year'] > 0), 'Sku'])
                 sending_store.transfer(index,list(sending_sku_set))
                 for index, _ in sending_store.store_rank.items():
                     if sending_store.store_total_addition[index] < total_receiving_number:
                         #transfer the remaining SKUs to this receiving store B
-                        remaining_sku_set = set(sending_store.one_store.loc[sending_store.one_store.OH > 0, 'Sku']) \
-                                        - set(sending_store.all_store_same_region.loc[(sending_store.all_store_same_region.PA_Store == sending_store.one_store.loc[0, 'Sending_Store']) & \
-                                             (sending_store.all_store_same_region['PA for the year'] > 0), 'Sku'])
+                        remaining_sku_set = set(sending_store.one_store.loc[:, 'Sku']) - sending_sku_set                     
                         sending_store.transfer(index,remaining_sku_set)
                         return sending_store
                     else:
                         continue
-# create the function to output the 
-def output_stores(*stores):
+        else:
+            continue
+
+# create the function to output the transferred info
+#consolidated_stores is a list here
+def output_stores(consolidated_stores):
     store_list = []
-    for store in stores:
+    for store in consolidated_stores:
         tem_store_copy = store.one_store_copy.copy()
-        tem_store_output = store.one_store_copy.loc[:,['Sending_Store', 'Style', 'Sku', 'OH']]
+        tem_store_output = store.one_store_copy.loc[:,['Sending_Store', 'Sku', 'OH']]
         for store_id,sku_list in store.receiving_store.items():
             tem_store_copy['Sku'] = np.where(tem_store_copy['Sku'].isin(sku_list), store_id, tem_store_copy['Sku'])
         tem_store_output['Receiving_Store'] = tem_store_copy['Sku']
         tem_store_output['Qty'] = tem_store_output['OH']
+        tem_store_output['Style'] = np.nan
         tem_store_output['Choice'] = np.nan
         tem_store_output['Cost'] = np.nan
         tem_store_output['Retail'] = np.nan
@@ -158,12 +152,27 @@ def output_stores(*stores):
         store_list.append(final_store_output)
     return pd.concat(store_list)
 
-#read in the file for consolidation
-def sending_receiving_stores():
-    pass
 
-#Client side to use the above class and methods
-test = consolidation(Sending_Info_group, PA_Info_group)
-output_stores(test).to_excel(os.path.expanduser("~\\OneDrive - Canadian Tire\\Desktop\\Consolidation_" + os.getlogin() + "_"
+#read in the excel sheet
+#NOTE: when you try to read excel in python, you should close any excel file you want to load first
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename()
+Sending_Info = pd.read_excel(file_path, sheet_name = "Sending Info").dropna(how='all', subset = ['Sending_Store', 'Region'])
+PA_Info = pd.read_excel(file_path, sheet_name = "PA Info").dropna(how='all', subset = ['PA_Store', 'Region'])
+#group same Sku together for both info sheet
+Sending_Info_group = Sending_Info.groupby(['Sending_Store','Sku','Region'],as_index = False)['OH'].sum()
+#remove the sending store id in the PA_Info sheet
+PA_Info = PA_Info.loc[~PA_Info.PA_Store.isin(set(Sending_Info['Sending_Store']))]
+PA_Info_group = PA_Info.groupby(['PA_Store','Sku','Region'],as_index = False)['PA for the year'].sum()
+display(Sending_Info_group.head(2))
+display(PA_Info_group.head(2))
+
+#do the actual consolidation store by store
+sending_store_list = []
+for key, group in Sending_Info_group.groupby('Sending_Store'):
+    sending_storeA = consolidation(group.reset_index(), PA_Info_group)
+    sending_store_list.append(sending_storeA)
+#generate the result
+output_stores(sending_store_list).to_excel(os.path.expanduser("~\\OneDrive - Canadian Tire\\Desktop\\test" + os.getlogin() + "_"
                    + date.today().strftime("%b") + date.today().strftime("%d") + ".xlsx"), index = False)
-
